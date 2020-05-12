21f6d298705541bb1e31bbe95ddfeec6ac6214f3
Jose Segura <00001617@uca.edu.sv>
2020-05-11 20:32:04 -0600

    Finish

---
+++ src/main/java/com/uca/capas/TareaLabo4NCapasApplication.java
@@ -0,0 +1,13 @@
+package com.uca.capas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TareaLabo4NCapasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TareaLabo4NCapasApplication.class, args);
+	}
+
+}
